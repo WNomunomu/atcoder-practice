599fcb85f738fa749824746beacce70f779bf232
WNomunomu <kohsei.nmr@gmail.com>
2025-09-23 19:14:52 +0900

    Single-Source-Shortest-Path

---
+++ AOJ/Single-Source-Shortest-Path.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <climits>
+using namespace std;
+
+struct Edge {
+  int to;
+  int cost;
+  Edge(int t, int c) : to(t), cost(c) {}
+};
+
+void dijkstra(vector<vector<Edge>>& graph, int start, vector<int>& dist) {
+  int n = graph.size();
+  dist.assign(n, INT_MAX);
+  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+  dist[start] = 0;
+  pq.push(make_pair(0, start));
+
+  while (!pq.empty()) {
+    int cost = pq.top().first;
+    int v = pq.top().second;
+    pq.pop();
+
+    if (cost > dist[v]) continue;
+
+    for (int i = 0; i < n; i++) {
+      Edge& e = graph[v][i];
+      if (dist[v] + e.cost < dist[e.to]) {
+        dist[e.to] = dist[v] = e.cost;
+        pq.push(make_pair(dist[e.to], e.to));
+      }
+    }
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<vector<Edge>> graph(n);
+
+  for (int i = 0; i < n; i++) {
+    int u, k;
+    cin >> u >> k;
+
+    for (int j = 0; j < k; j++) {
+      int v, c;
+      cin >> v >> c;
+      graph[u].push_back(Edge(v, c));
+    }
+  }
+
+  vector<int> dist;
+  dijkstra(graph, 0, dist);
+
+  for (int i = 0; i < n; i++) {
+    cout << i << " " << dist[i] << endl;
+  }
+
+  return 0;
+}
+
