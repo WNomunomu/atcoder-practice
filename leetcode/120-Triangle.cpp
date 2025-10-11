1924ac81b84fa8e0e37d306033513a6ff6635f73
WNomunomu <kohsei.nmr@gmail.com>
2025-10-11 19:10:40 +0900

    120-Triangle

---
+++ leetcode/120-Triangle.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+      int n = triangle.size();
+      int maxValue = 100000;
+
+      vector<vector<int>> dp(n, vector<int>(n, maxValue));
+
+      for (int i = 0; i < n; i++) {
+        dp[n - 1][i] = triangle[n - 1][i];
+      }
+
+      for (int i = n - 2; i >= 0; i--) {
+        for (int j = 0; j <= i; j++) {
+          dp[i][j] = min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j];
+        }
+      }
+      
+      return dp[0][0];
+    }
+};
+
