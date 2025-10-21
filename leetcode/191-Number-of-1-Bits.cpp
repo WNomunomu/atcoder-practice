0b2179d92fa8633d3ff5ea75d8b4e95a0e9d7c2d
WNomunomu <kohsei.nmr@gmail.com>
2025-10-21 21:26:08 +0900

    191-Number-of-1-Bits

---
+++ leetcode/191-Number-of-1-Bits.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int hammingWeight(int n) {
+      int ans = 0;
+
+      while (n > 0) {
+        ans += n % 2;
+        n /= 2;
+      }
+
+      return ans;
+    }
+};
+
